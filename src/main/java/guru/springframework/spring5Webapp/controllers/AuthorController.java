7ba1c8c50c6354f2dd9062a49127b53b9b7f4a11
Andre Bielser <abielser@sbcglobal.net>
2022-11-18 05:15:13 -0800

    Adding AuthorController

---
+++ src/main/java/guru/springframework/spring5Webapp/controllers/AuthorController.java
@@ -0,0 +1,27 @@
+package guru.springframework.spring5Webapp.controllers;
+
+import guru.springframework.spring5Webapp.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.persistence.Entity;
+
+@Controller
+public class AuthorController {
+    private final AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+
+        this.authorRepository = authorRepository;
+    }
+        @RequestMapping("/authors")
+        public String getAuthors(Model model) {
+            model.addAttribute("authors",authorRepository.findAll());
+
+            return "author/list";
+
+
+        }
+
+}
